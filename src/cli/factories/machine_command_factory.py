0fb41689a106034a924452dba2f32c9f9cee0c48
Flamur Gogolli <18527931+fgogolli@users.noreply.github.com>
2026-02-19 14:29:41 +0000

    fix: register ListMachinesQuery handler

--- src/cli/factories/machine_command_factory.py
+++ src/cli/factories/machine_command_factory.py
@@ -2,9 +2,8 @@
 
 from typing import Any, Optional
 
-from application.dto.queries import GetMachineQuery
+from application.dto.queries import GetMachineQuery, ListMachinesQuery
 from application.dto.bulk_queries import GetMultipleMachinesQuery
-from application.machine.queries import ListMachinesQuery
 from application.machine.commands import UpdateMachineStatusCommand
 
 
