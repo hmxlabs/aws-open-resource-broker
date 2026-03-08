e624f67600cf1ee1dab5ba5eac2f0db35d997e31
Flamur Gogolli <18527931+fgogolli@users.noreply.github.com>
2026-03-09 00:28:00 +0100

    refactor: delete dead ProviderStrategyResolver and ProviderSelection stack

--- src/orb/infrastructure/adapters/__init__.py
+++ src/orb/infrastructure/adapters/__init__.py
@@ -5,7 +5,6 @@ from .container_adapter import ContainerAdapter
 from .error_handling_adapter import ErrorHandlingAdapter
 from .factories.container_adapter_factory import ContainerAdapterFactory
 from .logging_adapter import LoggingAdapter
-from .provider_strategy_adapter import ProviderStrategyResolverAdapter
 from .storage_adapter import StorageReaderAdapter, StorageWriterAdapter
 from .template_configuration_adapter import TemplateConfigurationAdapter
 
@@ -15,7 +14,6 @@ __all__: list[str] = [
     "ContainerAdapterFactory",
     "ErrorHandlingAdapter",
     "LoggingAdapter",
-    "ProviderStrategyResolverAdapter",
     "StorageReaderAdapter",
     "StorageWriterAdapter",
     "TemplateConfigurationAdapter",
